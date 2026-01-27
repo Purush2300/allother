6583b376f0931a788ef8ef02e5d778be85c9f67a
reddypurushothm@gmail.com <reddypurushothm@gmail.com>
2026-01-26 23:28:34 -1200

    new

---
+++ Basicrec.java
@@ -0,0 +1,12 @@
+public class Basicrec{
+    static void fun1(int x,int y){
+      System.out.println(x+y);
+
+    }
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+     System.out.println("first");
+     fun1(a,b);
+    }
+}
